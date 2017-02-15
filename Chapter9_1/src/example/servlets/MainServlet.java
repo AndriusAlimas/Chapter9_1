7aa9a1e26ca4e44f20cd6f091d46684a73113bfa
Andrius <andriusjavait@gmail.com>
2017-02-15 21:03:54 +0000

    created skeleton servlet code MainServlet

---
+++ Chapter9_1/src/example/servlets/MainServlet.java
@@ -0,0 +1,11 @@
+package example.servlets;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class MainServlet extends HttpServlet {
+	public void doPost(HttpServletRequest request,
+			HttpServletResponse reponse)throws ServletException,IOException{
+		
+	}
+}
